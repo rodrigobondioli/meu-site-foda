8e5c566abaf3c6114c3d5cb2feef15660635cb3b
Rodrigo Bondioli <rodrigobondioli@MacBook-Pro-de-Rodrigo.local>
2025-05-14 09:42:34 -0300

    fix: registra plugin GSAP no nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['@/assets/css/global.css'],
+  plugins: ['~/plugins/gsap.client.ts'], // 👈 ESSE AQUI
   ssr: true
 })
