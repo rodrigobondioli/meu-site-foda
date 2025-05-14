ae76c5ba8e444a63803bc7bc6ab5bec4ce7ae581
Rodrigo Bondioli <rodrigobondioli@MacBook-Pro-de-Rodrigo.local>
2025-05-14 08:43:47 -0300

    fix: ativado SSR para compatibilidade com plugin GSAP

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['@/assets/css/global.css']
+  css: ['@/assets/css/global.css'],
+  ssr: true
 })
